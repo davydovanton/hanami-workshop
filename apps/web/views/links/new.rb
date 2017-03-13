9b2ce92657c02ae40b92890117c9544c2dc79bd6
Anton Davydov <antondavydov.o@gmail.com>
2017-03-13 19:20:02 +0300

    Create simple form for new links

--- apps/web/views/links/new.rb
+++ apps/web/views/links/new.rb
@@ -1,5 +1,13 @@
 module Web::Views::Links
   class New
     include Web::View
+
+    def form
+      form_for :link, routes.links_path do
+        text_field :url
+
+        submit 'Create'
+      end
+    end
   end
 end
--- spec/web/views/links/new_spec.rb
+++ spec/web/views/links/new_spec.rb
@@ -1,15 +1,9 @@
 require_relative '../../../../apps/web/views/links/new'
 
 RSpec.describe Web::Views::Links::New do
-  let(:exposures) { Hash[foo: 'bar'] }
+  let(:exposures) { {} }
   let(:template)  { Hanami::View::Template.new('apps/web/templates/links/new.html.slim') }
   let(:view)      { described_class.new(template, exposures) }
   let(:rendered)  { view.render }
 
-  it 'exposes #foo' do
-    pending 'This is an auto-generated test. Edit it and add your own tests.'
-
-    # Example
-    expect(view.foo).to eq exposures.fetch(:foo)
-  end
 end
