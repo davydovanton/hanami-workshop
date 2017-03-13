b314e8148f1159ef1a36ba2be4cb453a85977a08
Anton Davydov <antondavydov.o@gmail.com>
2017-03-13 19:34:10 +0300

    Create failed tests

--- spec/web/controllers/links/create_spec.rb
+++ spec/web/controllers/links/create_spec.rb
@@ -2,10 +2,44 @@ require_relative '../../../../apps/web/controllers/links/create'
 
 RSpec.describe Web::Controllers::Links::Create do
   let(:action) { described_class.new }
-  let(:params) { Hash[] }
+  let(:params) { { link: { url: '' } } }
 
-  it 'is successful' do
-    response = action.call(params)
-    expect(response[0]).to eq 200
+  let(:repo) { LinkRepository.new }
+
+  context 'when url is valid' do
+    let(:params) { { link: { url: 'http://google.com' } } }
+
+    it 'creates new link object' do
+      expect{ action.call(params) }.to change { repo.count }.by(1)
+      expect(repo.last.key).to_not be nil
+    end
+
+    it 'redirects to root page' do
+      it { expect(action.call(params)).to redirect_to('/') }
+    end
+
+    it 'displays link in flash' do
+      action.call(params)
+      flash = action.exposures[:flash]
+      it(flash[:info]).to match repo.last.key
+    end
+  end
+
+  context 'when url is invalid' do
+    let(:params) { { link: { url: 'http://google.com' } } }
+
+    it 'does not create new link object' do
+      expect{ action.call(params) }.to change { repo.count }.by(0)
+    end
+
+    it 'redirects to root page' do
+      it { expect(action.call(params)).to redirect_to('/') }
+    end
+
+    it 'displays error in flash' do
+      action.call(params)
+      flash = action.exposures[:flash]
+      it(flash[:error]).to eq 'invalid link'
+    end
   end
 end
