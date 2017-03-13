da0ed396321738215626bf61959c94869cd4216e
Anton Davydov <antondavydov.o@gmail.com>
2017-03-13 19:53:53 +0300

    Create action for saving link

--- apps/web/controllers/links/create.rb
+++ apps/web/controllers/links/create.rb
@@ -1,8 +1,24 @@
 module Web::Controllers::Links
   class Create
     include Web::Action
+    include Hanami::Action::Session
+
+    params do
+      required(:link).schema do
+        required(:url).filled(:str?)
+      end
+    end
 
     def call(params)
+      if params.valid?
+        params[:link][:key] = '123'
+        link = LinkRepository.new.create(params[:link])
+        flash[:info] = "your link: site.com/link/#{link.key}"
+      else
+        flash[:error] = 'invalid link'
+      end
+
+      redirect_to routes.root_path
     end
   end
 end
--- spec/web/controllers/links/create_spec.rb
+++ spec/web/controllers/links/create_spec.rb
@@ -10,36 +10,36 @@ RSpec.describe Web::Controllers::Links::Create do
     let(:params) { { link: { url: 'http://google.com' } } }
 
     it 'creates new link object' do
-      expect{ action.call(params) }.to change { repo.count }.by(1)
+      expect{ action.call(params) }.to change { repo.all.to_a.size }.by(1)
       expect(repo.last.key).to_not be nil
     end
 
     it 'redirects to root page' do
-      it { expect(action.call(params)).to redirect_to('/') }
+      expect(action.call(params)).to redirect_to('/')
     end
 
     it 'displays link in flash' do
       action.call(params)
       flash = action.exposures[:flash]
-      it(flash[:info]).to match repo.last.key
+      expect(flash[:info]).to match repo.last.key
     end
   end
 
   context 'when url is invalid' do
-    let(:params) { { link: { url: 'http://google.com' } } }
+    let(:params) { { link: { url: '' } } }
 
     it 'does not create new link object' do
-      expect{ action.call(params) }.to change { repo.count }.by(0)
+      expect{ action.call(params) }.to change { repo.all.to_a.size }.by(0)
     end
 
     it 'redirects to root page' do
-      it { expect(action.call(params)).to redirect_to('/') }
+      expect(action.call(params)).to redirect_to('/')
     end
 
     it 'displays error in flash' do
       action.call(params)
       flash = action.exposures[:flash]
-      it(flash[:error]).to eq 'invalid link'
+      expect(flash[:error]).to eq 'invalid link'
     end
   end
 end
