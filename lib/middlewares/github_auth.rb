9c17402a1deb7f60508ef60a531a0fd32c4a1a04
Anton Davydov <antondavydov.o@gmail.com>
2017-03-17 03:39:31 +0300

    Fix middleware

--- lib/middlewares/github_auth.rb
+++ lib/middlewares/github_auth.rb
@@ -4,23 +4,23 @@ class GithubAuth < Roda
   plugin :middleware
 
   route do |r|
+    r.is 'auth/failure' do
+      # logging errors
+    end
+
+    r.is 'auth/logout' do
+      session[:account] = nil
+      r.redirect "/admin"
+    end
+
     r.on 'auth/:provider' do
       r.is 'callback' do
         login = request.env['omniauth.auth']['extra']['raw_info']['login']
         repo = AccountRepository.new
         session[:account] = repo.find_by_login(login) || repo.create(login: login)
 
-        r.redirect "/"
+        r.redirect "/admin"
       end
-    end
-
-    r.is 'failure' do
-      # logging errors
-    end
-
-    r.is 'logout' do
-      session[:account] = nil
-      r.redirect "/"
     end
   end
 end
